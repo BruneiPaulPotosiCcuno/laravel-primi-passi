7aab4c37c12f807adbd2c5342ea0105432078343
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-05-22 21:35:46 +0200

    add-dati

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        HELLO WORLD
+    </h1>
+    {{-- QUESTO E IL TEST PER I DATI --}}
+    <ul>
+        <li>Ciao {{$name}} {{$lastname}}</li>
+        <li>Tu hai {{$age}} anni</li>
+        <li>Questo e il tuo email: {{$email}}</li>
+        <li>Questo e il tuo n di telefono{{$phone}}</li>
+        <li>Abiti a {{$city}}</li>
+        <li>Residente a {{$country}}</li>
+    </ul>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routesv
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/homepage', function () {
+    $data = [
+    'name'=>'Paul',
+    'lastname'=>'Brunei',
+    'age'=>'24',
+    'email'=>'neoxtrotoka@gmail.com',
+    'phone'=>'32132141512',
+    'city'=>'Torino',
+    'country'=>'Italia',
+    ];
+    return view('home', $data);
 });
